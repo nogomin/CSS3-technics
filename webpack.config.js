176d6ed47765fc03257795da620ca4952e7bae64
nogomin <tttime0@gmail.com>
2019-06-04 21:41:09 +0900

    WordRelay (type : class), Add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     name: 'word-relay-setting',
     mode: 'development', //실서비스 : production
-    devtool: 'eval', //빠르게..
+    devtool: 'eval', //개발용, hidden-source-map : 배포용
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -15,14 +16,29 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'], // class react에 constructor없이 state = {} 쓸수 있게 해준다
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: { //browserslist <--구글 검색
+                            browsers: ['> 1% in KR', 'last 2 chrome versions'], //한국에서 점유율 1%이상, 최신 2단계 크롬 버전
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react'
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties', // class react에 constructor없이 state = {} 쓸수 있게 해준다
+                    'react-hot-loader/babel', // hot-loader 사용시 반드시 추가할것
+                ], 
             }
         }],
     },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({debug: true}), //loader options에 다 debug: true를 자동으로 넣어준다
+    ],
     output: {
         path: path.join(__dirname, 'dist'), //path.join는 경로를 합쳐준다. __dirname: 현재폴더
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/', // dist 폴더에 빌드하고 싶으면 설정, 설정 후 반드시 npm run dev 재시작하기
     },
 
 }

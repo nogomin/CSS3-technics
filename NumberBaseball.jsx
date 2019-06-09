a45436f643c9534c1725b4268d21169ac1d426e1
nogomin <tttime0@gmail.com>
2019-06-09 11:26:19 +0900

    Add NumberBaseball Core Logic

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -1,35 +1,81 @@
 import React, { Component } from 'react';
 import Try from './Try';
+
 // const React = require('react');
 // const { Component } = React;
 
-function getNumber() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
-
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class NumberBaseball extends Component {
     state = {
         result: '',
         value: '',
-        answer: getNumber(),
+        answer: getNumbers(),
         tries: [],
     };
     
-    onSubmitForm = () => {
-
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런!',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!'}]
+                /* 
+                this.state.tries.push하면 기존 배열에서 달라지는것을 react에서 알아차리지 못함
+                그래서 기존 꺼를 ...this.state.tries로 복사하고 새로운 값을 더해준다. 그럼 기존 배열과 새로생긴배열이 false가 되어
+                리액트가 기존 배열이 달라졌음을 알수 있음
+                */
+            })
+            alert('홈런! 게임을 다시 시작합니다');
+            this.setState({
+                value:'',
+                answer: getNumbers(),
+                tries: [],
+            });
+        } else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) {
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다`,
+                });
+                alert('게임을 다시 시작합니다')
+                this.setState({
+                    value:'',
+                    answer: getNumbers(),
+                    tries: [],
+                });
+            } else {
+                for (let i =0; i < 4; i += 1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
+                    value: '',
+                })
+            }
+        }
     };
 
-    onChangeInput = () => {
-
+    onChangeInput = (e) => {
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        })
     };
-
-    fruits = [
-        { fruit: '사과', taste: '맛있다'},
-        { fruit: '딸기', taste: '상큼하다'},
-        { fruit: '수박', taste: '달다'},
-        { fruit: '참외', taste: '별로다'},
-        { fruit: '복숭아', taste: '최애'},
-    ];
 
     render() {
         return (
@@ -40,9 +86,9 @@ class NumberBaseball extends Component {
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {this.fruits.map((v, i) => {
+                    {this.state.tries.map((v, i) => {
                         return (
-                            <Try key={v.fruit + v.taste} value={v} index={i} />
+                            <Try key={`${i + 1}차 시도 :`} tryInfo={v} />
                             /*
                             key안에는 고유한 값을 넣어야한다. 위와 같은 경우 fruit가 중복값이 발생할수 있으므로 taste를 합쳐주었다.
                             고민이 필요한 부분. 인덱스 i가 고유값이긴하나 key값에 i를 넣지않도록한다. 이유는 성능최적화 문제 + react에서 key
--- Try.jsx
+++ Try.jsx
@@ -4,13 +4,9 @@ class Try extends Component {
     render() {
         return (
             <li>
-                <b>{this.props.value.fruit}</b> - {this.props.index}
-                <div>컨텐츠</div>
-                <div>컨텐츠1</div>
-                <div>컨텐츠2</div>
-                <div>컨텐츠3</div>
+                <div>{this.props.tryInfo.try}</div>
+                <div>{this.props.tryInfo.result}</div>
             </li>
-            
         )
     }
 }

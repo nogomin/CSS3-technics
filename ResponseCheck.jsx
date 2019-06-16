d1d13c0e40f0d5a51f21a46187d7c16453341270
nogomin <tttime0@gmail.com>
2019-06-16 20:17:49 +0900

    반응속도체크 (Hooks)

--- ResponseCheck.jsx
+++ ResponseCheck.jsx
@@ -1,79 +1,59 @@
-import React, {Component} from 'react'
+import React, {useState, useRef} from 'react'
 
-class ResponseCheck extends Component {
-    state = {
-        state: 'waiting',
-        message: '클릭해서 시작하세요',
-        result: [], 
-    };
+const ResponseCheck = () => {
+    const [state, setState] = useState('waiting');
+    const [message, setMessage] = useState('클릭해서 시작하세요');
+    const [result, setResult] = useState([]);
+    const timeout = useRef(null); //값이 바뀌기기는 하지만 화면에 영향을 주고 싶지 않을때 useRef 사용
+    const startTime = useRef();
+    const endTime = useRef();
 
-    timeout; // setTimeout 초기화를 위한 변수
-    startTime;
-    endTime;
-
-    onClickScreen = () => {
-        const { state, message, result} = this.state;
+    const onClickScreen = () => {
         if(state === 'waiting') {
-            this.setState({
-                state: 'ready',
-                message: '초록색이 되면 클릭하세요',
-            })
-            this.timeout = setTimeout(()=>{
-                this.setState({
-                    state: 'now',
-                    message: '지금 클릭'
-                })
-                this.startTime = new Date();
+            setState('ready');
+            setMessage('초록색이 되면 클릭하세요');
+            timeout.current = setTimeout(()=>{
+                setState('now');
+                setMessage('지금 클릭');
+                startTime.current = new Date();
             }, Math.floor(Math.random() * 1000) + 2000); //2 ~3초 랜덤
         } else if (state === 'ready') { // 성급하게 클릭
-            clearTimeout(this.timeout);
-            this.setState({
-                state: 'waiting',
-                message: '이런 성급하셨군요! 초록색이 된 후에 클릭하세요'
-            })
-
+            clearTimeout(timeout.current);
+            setState('waiting');
+            setMessage('이런 성급하셨군요! 초록색이 된 후에 클릭하세요');
         } else if (state === 'now') { // 반응속도 체크
-            this.endTime = new Date();
-            this.setState((prevState) => {
-                return {
-                    state: 'waiting',
-                    message: '클릭해서 시작하세요',
-                    result: [...prevState.result, this.endTime - this.startTime],
-                }
-            })
+            endTime.current = new Date();
+            setState('waiting');
+            setMessage('클릭해서 시작하세요');
+            setResult((prevResult) => {
+                return [...prevResult, endTime.current - startTime.current]; //useRef면 무조건 current로 접근
+            });
         }
+    };
+    const onReset = () => {
+        setResult([]);
     }
-
-    onReset = () => {
-        this.setState({
-            result: [],
-        })
-    }
-
-    renderAverage = () => {
-        const {result} = this.state;
+    const renderAverage = () => {
         return result.length === 0
             ? null // jsx에서는 null은 태그가 없음을 의미
             : <>
-            <div>평균 시간 : {this.state.result.reduce((a,c) => a + c) / this.state.result.length}ms</div>
-            <button onClick={this.onReset}>리셋</button>
+            <div>평균 시간 : {result.reduce((a,c) => a + c) / result.length}ms</div>
+            <button onClick={onReset}>리셋</button>
             </>
     };
 
-    render() {
-        return (
-            <>
-                <div
-                    id="screen"
-                    className={this.state.state}
-                    onClick={this.onClickScreen}
-                >
-                    {this.state.message}
-                </div>
-                {this.renderAverage()}                
-            </>
-        )
-    }
+    return (
+        <>
+            <div
+                id="screen"
+                className={state}
+                onClick={onClickScreen}
+            >
+                {message}
+            </div>
+            {renderAverage()}                
+        </>
+    );
 }
 
 export default ResponseCheck;
